b566a0ac313b86f7b2f781eb9fb815b91ebb92ae
Raj <raju.11811924@lpu.in>
2021-09-26 19:18:47 +0000

    Main fixed

--- Love_Babbar_Sheet/Arrays/Main.java
+++ Love_Babbar_Sheet/Arrays/Main.java
@@ -9,15 +9,8 @@ public class Main{
         // }System.out.println();
 
         // Calling FindMaxMin
-        // int[] arr = {1,2,3,4,5,6,7,8};
-        // System.out.println(FindMaxMin.find(arr)[0] + " " + FindMaxMin.find(arr)[1]);
+        int[] arr = {1,2,3,4,5,6,7,8};
+        System.out.println(FindMaxMin.find(arr)[0] + " " + FindMaxMin.find(arr)[1]);
 
-        // Calling Sort012
-        int[] arr = {2,1,0,0,1,0,2};
-        Sort012.sort(arr);
-
-        for (int elem : arr) {
-            System.out.print(elem + " ");
-        }System.out.println();
     }
 }
