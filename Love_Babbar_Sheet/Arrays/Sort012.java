b4d4211f4c3abb9d90f9fef41992ae397e64c603
Raj <raju.11811924@lpu.in>
2021-09-26 19:19:41 +0000

    Arrays > 4.Sort012

---
+++ Love_Babbar_Sheet/Arrays/Sort012.java
@@ -0,0 +1,22 @@
+public class Sort012{
+    static void sort(int[] arr){
+        int zeroIndex = 0;
+        int twoIndex = arr.length - 1;
+        int travIndex = 0;
+
+        while(travIndex <= twoIndex){
+            if(arr[travIndex] == 0){
+                int temp = arr[travIndex];
+                arr[travIndex] = arr[zeroIndex];
+                arr[zeroIndex] = temp;
+
+                zeroIndex += 1;
+            }else if(arr[travIndex] == 2){
+                int temp = arr[travIndex];
+                arr[travIndex] = arr[twoIndex];
+                arr[twoIndex] = temp;
+            }
+            travIndex += 1;
+        }
+    }
+}
