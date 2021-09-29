253f9dc4806db79d500ad1f8932c9228badf84cd
Raj <raju.11811924@lpu.in>
2021-09-29 15:15:09 +0530

    Searching&Sorting > 3.BubbleSort (Modified)

--- CodingNinjas/SearchingAndSorting/BubbleSort.java
+++ CodingNinjas/SearchingAndSorting/BubbleSort.java
@@ -1,14 +1,13 @@
 public class BubbleSort{
     static void sort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j < arr.length; j++) {
-                System.out.print(arr[j] + " ");
-                if(arr[j] < arr[j - 1]){
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j + 1]){
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
-            }System.out.println();
+            }
         }
     }
 }
